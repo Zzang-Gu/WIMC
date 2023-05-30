4543eba8f08805334a0cb385ca4f0a3bbee0780a
Junwon Moon <diayoak@kakao.com>
2023-05-30 20:42:09 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,29 @@
-from lobby import Lobby
 import customtkinter
+class MyFrame(customtkinter.CTkFrame):
+    def __init__(self, master, **kwargs):
+        super().__init__(master, **kwargs)
 
-customtkinter.set_appearance_mode("System")
-customtkinter.set_default_color_theme("blue")
+        self.label1 = customtkinter.CTkLabel(self, text="What's in My Closet2", fg_color="#ffffff",
+                                             text_color="#000000",
+                                             anchor="center", font=("Arial", 64), width=300, height=200)
+        self.label1.grid(row=0, column=0)
 
-app = Lobby()
+        self.label2 = customtkinter.CTkLabel(self, text="What's in My Closet1", fg_color="#ffffff", text_color="#000000", anchor="center", font=("Arial", 64), width=1400, height=500)
+        self.label2.grid(row=0, column=0)
+
+
+
+
+class App(customtkinter.CTk):
+    def __init__(self):
+        super().__init__()
+        self.geometry("1400x720")
+        self.grid_rowconfigure(0, weight=1)
+        self.grid_columnconfigure(0, weight=1)
+
+        self.my_frame = MyFrame(master=self)
+        self.my_frame.grid(row=0, column=0, padx=0, pady=0, sticky="nsew")
+
+
+app = App()
 app.mainloop()
